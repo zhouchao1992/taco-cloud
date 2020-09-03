843ab0637be9c5f6f24eb5defd057f148846787f
zhouchao1992 <1669028439@qq.com>
2020-09-03 10:35:00 +0800

    阿道夫撒

--- taco-cloud.sh
+++ taco-cloud.sh
@@ -4,11 +4,9 @@ docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
-docker rmi `docker images | grep none | awk '{print $3}'`
-echo '----rm none images----'
 docker run -p 8887:8887 --name ${app_name} \
 --link mysql:db \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
--d ${app_name}:1.0-SNAPSHOT
+-d mall-tiny/${app_name}:1.0-SNAPSHOT
 echo '----start container----'
