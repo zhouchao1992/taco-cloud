ee69ae7d7bc9bf96a35389af027277bea5206b6f
zhouchao1992 <1669028439@qq.com>
2020-09-03 16:39:30 +0800

    重新配置镜像文件打包1

--- taco-cloud.sh
+++ taco-cloud.sh
@@ -4,9 +4,9 @@ docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
-docker run -p 8887:8887 --name ${app_name} \
+docker run -p 8088:8088 --name ${app_name} \
 --link mysql:db \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
--d taco-cloud/${app_name}:1.0-SNAPSHOT
+-d zc/${app_name}:1.0-SNAPSHOT
 echo '----start container----'
