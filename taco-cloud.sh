4c6a3e66b9428386a44947a6358410c2b7a1a369
zhouchao1992 <1669028439@qq.com>
2020-09-03 17:02:32 +0800

    修改端口

--- taco-cloud.sh
+++ taco-cloud.sh
@@ -4,9 +4,9 @@ docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
-docker run -p 8088:8088 --name ${app_name} \
+docker run -p 8883:8883 --name ${app_name} \
 --link mysql:db \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
--d zc/${app_name}:1.0-SNAPSHOT
+-d zc/${app_name}:0.0.1-SNAPSHOT
 echo '----start container----'
